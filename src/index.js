25947718fe141e0a0fa48eabb7f6c97e51e5a632
zlargon <zlargon@icloud.com>
2016-04-08 16:55:04 +0800

    setup web app development environment
    https://github.com/zlargon/my-web-app-boilerplate

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'normalize.css/normalize.css'; // v4.0.0: missing `main` in package.json
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
